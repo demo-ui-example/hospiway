1091d4555f62cae3395e43ce68f52397b516cef6
HuuBang11 <huubang0312@gmail.com>
2025-11-21 20:29:06 +0700

    first commit

---
+++ assets/js/index/script.js
@@ -0,0 +1,37 @@
+import { preloadImages } from "../../libs/utils.js";
+("use strict");
+$ = jQuery;
+// setup lenis
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+// end lenis
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+};
+preloadImages("img").then(() => {
+  // Once images are preloaded, remove the 'loading' indicator/class from the body
+
+  init();
+});
+
+// loadpage
+let isLinkClicked = false;
+$("a").on("click", function (e) {
+  // Nếu liên kết dẫn đến trang khác (không phải hash link hoặc javascript void)
+  if (this.href && !this.href.match(/^#/) && !this.href.match(/^javascript:/)) {
+    isLinkClicked = true;
+    console.log("1");
+  }
+});
+
+$(window).on("beforeunload", function () {
+  if (!isLinkClicked) {
+    $(window).scrollTop(0);
+  }
+  isLinkClicked = false;
+});
